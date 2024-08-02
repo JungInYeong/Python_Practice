4ac3b76db985e5aa6e93bba0b7b0eaa1003aeca6
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-08-02 15:48:47 +0900

    0802_01

---
+++ 0802/0802_01.py
@@ -0,0 +1,14 @@
+# print("안녕하세요")
+# 실습3
+#name = input("이름을 입력하세요. ")
+#age = input("나이를 입력하세요. ")
+#print("안녕하세요! "+name+"님 ("+age+"세)")
+
+# 실습 4
+num1 = input()
+num2 = input()
+
+print(int(num1)*int(num2[2]))
+print(int(num1)*int(num2[1]))
+print(int(num1)*int(num2[0]))
+print(int(num1)*int(num2))
---
+++ 0802/0802_01shell.py
@@ -0,0 +1,67 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+안녕하세요
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+이름을 입력하세요. 홍길동
+나이를 입력하세요. 100
+안녕하세요! 홍길동 님 ( 100 )
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+이름을 입력하세요. 홍길동
+나이를 입력하세요. 100
+안녕하세요! 홍길동 님 ( 100 세)
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+이름을 입력하세요. 홍길동
+나이를 입력하세요. 100
+안녕하세요!홍길동님 (100 세)
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+이름을 입력하세요. 홍길동
+나이를 입력하세요. 100
+안녕하세요! 홍길동님 (100세)
+
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+이름을 입력하세요. 
+Traceback (most recent call last):
+  File "C:/Python_Practice/0802/0802_01.py", line 3, in <module>
+    name = input("이름을 입력하세요. ")
+KeyboardInterrupt
+>>> 
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+472
+385
+Traceback (most recent call last):
+  File "C:/Python_Practice/0802/0802_01.py", line 11, in <module>
+    print(num1*num2[2])
+TypeError: 'int' object is not subscriptable
+>>> 
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+472
+385
+Traceback (most recent call last):
+  File "C:/Python_Practice/0802/0802_01.py", line 11, in <module>
+    print(num1*(num2[2]))
+TypeError: 'int' object is not subscriptable
+>>> 
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+472
+385
+2360
+3776
+1416
+Traceback (most recent call last):
+  File "C:/Python_Practice/0802/0802_01.py", line 14, in <module>
+    print(num1*num2)
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> 
+================== RESTART: C:/Python_Practice/0802/0802_01.py =================
+472
+385
+2360
+3776
+1416
+181720
